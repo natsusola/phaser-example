a67d5f330a2d2393931ebe9a1a705195f8534a73
Natsu <natsu@jcd.tw>
2018-02-06 16:53:36 +0800

    新增範例 2

--- sample_1/src/js/index.js
+++ sample_1/src/js/index.js
@@ -1,9 +1,9 @@
-window.log = console.log.bind(this, `%c LOG `, 'background: #FF7818; color: white');
-if (!__DEV__) console.log = () => {};
-
-import PIXI from 'expose-loader?PIXI!phaser-ce/build/custom/pixi.js';
-import p2 from 'expose-loader?p2!phaser-ce/build/custom/p2.js';
-import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
+// window.log = console.log.bind(this, `%c LOG `, 'background: #FF7818; color: white');
+// if (!__DEV__) console.log = () => {};
+//
+// import PIXI from 'expose-loader?PIXI!phaser-ce/build/custom/pixi.js';
+// import p2 from 'expose-loader?p2!phaser-ce/build/custom/p2.js';
+// import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
 
 let game = new Phaser.Game(800, 600, Phaser.AUTO, null, {
   preload, create, update
