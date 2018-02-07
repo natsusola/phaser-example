a1e72c0ac1cf164c15e5c5933c3a5556b3e21847
Natsu <natsu@jcd.tw>
2018-02-07 17:46:33 +0800

    修正貼圖邊界

--- sample_2/src/js/index.js
+++ sample_2/src/js/index.js
@@ -22,10 +22,11 @@ function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
   player = game.add.sprite(0, game.world.height - 150, 'hero');
-  player.anchor.set(0.5);
+  player.anchor.set(0, 1);
   game.physics.enable(player, Phaser.Physics.ARCADE);
   player.body.gravity.y = 3000;
   player.body.collideWorldBounds = true;
+  // player.body.linearDamping = 1;
   player.animations.add('stand', Phaser.Animation.generateFrameNames('stand_0', 1, 2, '.png'), 3, this);
   player.animations.add('walk', Phaser.Animation.generateFrameNames('walk_0', 1, 3, '.png'), 9, this);
   player.animations.add('jump', Phaser.Animation.generateFrameNames('jump_0', 1, 2, '.png'), 1, this);
@@ -55,15 +56,17 @@ function update() {
   player.body.setSize(player.width * player.scale.x, player.height);
   game.physics.arcade.collide(player, platforms);
   player.body.velocity.x = 0;
-
+  
 
   if (zKey.isDown) {
     player.animations.play('attack1')
   } else if (cursors.left.isDown) {
+    if (player.scale.x === 1) player.x += player.width;
     player.scale.x = -1;
     player.body.velocity.x = -200;
     if (player.body.touching.down) player.animations.play('walk');
   } else if (cursors.right.isDown) {
+    if (player.scale.x === -1) player.x += player.width;
     player.scale.x = 1;
     player.body.velocity.x = 200;
     if (player.body.touching.down) player.animations.play('walk');
